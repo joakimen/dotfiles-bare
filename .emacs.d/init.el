962d27fc172d093b995dc2933e162b34b1e29548
Joakim Engeset <joakim.engeset@gmail.com>
2019-08-23 14:48:24 +0200

    initial emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,11 @@
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
